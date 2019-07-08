22f1b7354a0a55bb76ff0f5ad48cbf07c7ab12e4
Flatiron School <email.the.anderson@gmail.com>
2019-07-08 18:01:17 -0400

    rebuilt scaffolding

---
+++ db/migration/03_create_assignments.rb
@@ -0,0 +1,8 @@
+class CreateAssignments.rb < ActiveRecord::Migration[5.0]
+
+    def change 
+        create_table :assignments do |t|
+            t.integer :user_id
+            t.integer :chore_id
+
+end 

e2930e8aaa05b9776848bbcc98ed440017a4cd5f
Flatiron School <email.the.anderson@gmail.com>
2019-07-08 19:16:22 -0400

    corrected syntax error in 03_create_assignment

--- db/migration/03_create_assignments.rb
+++ db/migration/03_create_assignments.rb
@@ -4,5 +4,8 @@ class CreateAssignments.rb < ActiveRecord::Migration[5.0]
         create_table :assignments do |t|
             t.integer :user_id
             t.integer :chore_id
+            t.string :due_by
+        end
+    end 
 
 end 

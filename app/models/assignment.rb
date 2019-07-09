b5d4527ea9b301325873abb9149bfae8208b028e
Flatiron School <email.the.anderson@gmail.com>
2019-07-09 15:13:11 -0400

    chore wheel only assigns tasks unique to user

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,8 +8,13 @@ class Assignment < ActiveRecord::Base
         chores = Chore.all 
         lucky_user = users.sample
         nasty_job = chores.sample
-        Assignment.create(user: lucky_user, chore: nasty_job, taskname: nasty_job.name)
-        puts "#{lucky_user.name} has to #{nasty_job.name.downcase}."
+        already_doing = lucky_user.similar_task(lucky_user, nasty_job.name)
+        if already_doing == true 
+            Assignment.create(user: lucky_user, chore: nasty_job, taskname: nasty_job.name)
+            puts "#{lucky_user.name} has to #{nasty_job.name.downcase}."
+        else 
+            puts "The wheel says #{lucky_user.name} has to #{nasty_job.name.downcase} but they already have to do that!"
+        end
     end 
 
 end 

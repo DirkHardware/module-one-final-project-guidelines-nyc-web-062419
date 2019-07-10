cd7e12cc2f1b8bdec53ce60116174d690512ca3c
Flatiron School <email.the.anderson@gmail.com>
2019-07-09 22:24:20 -0400

    expanded CLI

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -26,16 +26,32 @@ class CLI
             Assignment.who_does_what
             intro 
         when "shirk"
-            puts "Who doesn't want their job?"
-            username = gets.chomp
-            user = User.find_by name: username.capitalize
-            puts "What job do they not want to do? e.g. 'Do the dishes'"
-            chorename = gets.chomp
-            puts "Who should do it instead?"
-            suckername = gets.chomp
-            sucker = User.find_by name: suckername.capitalize
-            # binding.pry 
-            user.shirk_task(sucker, chorename)
+            shirk
+            # puts "Who doesn't want their job?"
+            # username = gets.chomp
+            # user = User.find_by name: username.capitalize
+            # if user == nil
+            #     puts "Sorry but they don't live here. Type another name."
+            # puts "What job do they not want to do? e.g. 'Do the dishes'"
+            # chorename = gets.chomp
+            # puts "Who should do it instead?"
+            # suckername = gets.chomp
+            # sucker = User.find_by name: suckername.capitalize
+            # # binding.pry 
+            # user.shirk_task(sucker, chorename)
+            intro
+
+        when "spin the wheel"
+            spin_the_wheel
+            intro 
+
+        when "finish"
+            finish
+            intro
+
+        when "finish all"
+            finish_all
+            intro
 
         when "exit"
             break_loop
@@ -47,6 +63,68 @@ class CLI
 
     def break_loop
     end 
+
+    def shirk 
+        puts "Who doesn't want their job?"
+        username = gets.chomp
+        user = User.find_by name: username.capitalize
+        if user == nil
+            puts "Sorry but they don't live here. Type another name."
+            shirk 
+        end 
+        puts "What job do they not want to do? e.g. 'Do the dishes'"
+        chorename = gets.chomp
+        puts "Who should do it instead?"
+        suckername = gets.chomp
+        sucker = User.find_by name: suckername.capitalize
+        # binding.pry 
+        user.shirk_task(sucker, chorename)
+    end 
+
+    def spin_the_wheel
+        puts "Who wants to spin the wheel!"
+        username = gets.chomp
+        user = User.find_by name: username.capitalize
+        if user == nil
+            puts "Sorry but they don't live here. Type another name."
+            spin_the_wheel
+        end
+        Assignment.chore_wheel(user)
+    end
+
+    def finish
+        puts "Who finished their task?"
+        username = gets.chomp
+        user = User.find_by name: username.capitalize
+        if user == nil 
+            puts "Sorry but they don't live here. Type another name."
+            finish
+        end
+        puts "Awesome! What did you finish?"
+        taskname = gets.chomp 
+        chore = Chore.find_by name: taskname.capitalize
+        if chore == nil
+            puts "That wasn't on the list. Try again."
+            finish
+        end
+        taskname = chore.name
+        user.complete_task(taskname)
+        puts "#{user.name} completed task #{taskname}!"
+    end
+
+    def finish_all 
+        puts "Who finished their tasks?"
+        username = gets.chomp
+        user = User.find_by name: username.capitalize
+        if user == nil 
+            puts "Sorry but they don't live here. Type another name."
+            finish
+        end
+        user.finish_all_chores
+        puts "#{user.name} has completed all their tasks!"
+    end
+
+
 
 
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,17 +3,17 @@ class Assignment < ActiveRecord::Base
     belongs_to :user
     belongs_to :chore
 
-    def self.chore_wheel
-        users = User.all
+    def self.chore_wheel(user)
+        lucky_user = user
         chores = Chore.all 
-        lucky_user = users.sample
         nasty_job = chores.sample
         already_doing = lucky_user.similar_task(lucky_user, nasty_job.name)
         if already_doing == true 
             Assignment.create(user: lucky_user, chore: nasty_job, taskname: nasty_job.name)
             puts "#{lucky_user.name} has to #{nasty_job.name.downcase}."
         else 
-            puts "The wheel says #{lucky_user.name} has to #{nasty_job.name.downcase} but they already have to do that!"
+            puts "The wheel says #{lucky_user.name} has to #{nasty_job.name.downcase} but they already have to do that! Lets try again!"
+            Assignment.chore_wheel(lucky_user)
         end
     end 
 
